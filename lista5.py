34efafb90c17fb66a1addd5a606a7300f15fea4a
Amanjot <Amanjot@localhost.localdomain>
2018-03-04 20:19:56 +0100

    lista5

---
+++ lista5.py
@@ -0,0 +1,20 @@
+#coding: utf-8
+
+numero = int(input("Dígame cuántas palabras tiene la lista: "))
+palabra=['Carmen', 'Carmen', 'Alberto', 'Benito', 'David']
+if numero < 1:
+    print("¡Imposible!")
+else:
+  cant=int(input("Ingrese las palabras para la lista:"))
+for i in range (1,cant+1):
+  palabra1=input("Ingrese la palabra")
+  palabra.append(palabra1)
+print ("La lista creada es:",palabra)
+elim= input("eliminar la palabra: ")
+for x in range(len(lista)):  
+
+     while y in numero:
+
+   numero.remove(x)
+
+print("La lista es ahora:",lista)
